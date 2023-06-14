708bb6a39629e8c37b3b1af7e0f19b534b844601
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-06-14 17:14:45 +0600

    useadmin hook has been created

--- src/Utilities/useClasses.jsx
+++ src/Utilities/useClasses.jsx
@@ -2,18 +2,16 @@
 import { useContext } from 'react';
 import { useQuery } from 'react-query';
 import { AuthContext } from '../Providers/AuthProvider';
+import useAxiosSecure from './useAxiosSecure';
 
 const useClasses = () => {
     const { user } = useContext(AuthContext)
-    const token=localStorage.getItem('jwt-secret-token')
-
+    const [axiosSecure]= useAxiosSecure()
     const { isLoading,  data, refetch} = useQuery({
         queryKey:['classes', user?.email],
         queryFn: async() =>{
-            const response= await fetch(`http://localhost:5000/myclasses?email=${user.email}`, { headers:{
-                authorization:`bearer:${token}`
-            }})
-            return response.json()
+            const response= await axiosSecure(`/myclasses?email=${user.email}`)
+            return response.data;
         } ,
     
     })
